4034a12c50b5fa2bd50690a4ac129be8d967ed67
CristianRiveraEsparza <cristianriveraesparza890@gmail.com>
2022-10-24 18:01:10 -0500

    Agregar archivos de proyecto.

---
+++ BackEnd-ejemplo1/Services/PeliculasService.cs
@@ -0,0 +1,30 @@
+﻿using BackEnd_ejemplo1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BackEnd_ejemplo1.Services
+{
+    public class PeliculasService
+    {
+        public List<Pelicula> ObtenerPelicula()
+        {
+            var pelicula = new Pelicula()
+            {
+                Titulo = "XD",
+                Duracion = 150,
+                Pais = "Usa",
+                Publicacion = new DateTime(2012, 12, 11)
+            };
+            var pelicula2 = new Pelicula()
+            {
+                Titulo = "XDdd",
+                Duracion = 130,
+                Pais = "Usa",
+                Publicacion = new DateTime(2015, 13, 12)
+            };
+            return new List<Pelicula> { pelicula, pelicula2 };
+        }
+    }
+}
